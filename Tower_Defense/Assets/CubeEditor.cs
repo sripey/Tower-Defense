c9e8eb2c4926ee8dadfadb390cd4f2e79a14abfd
sripey <sripe2@live.com>
2018-06-07 11:40:54 -0400

    Sandbox Cube 0.1
    cube snaps to grid.
    grid location displayed on top of cube.

--- Tower_Defense/Assets/CubeEditor.cs
+++ Tower_Defense/Assets/CubeEditor.cs
@@ -3,18 +3,28 @@ using System.Collections.Generic;
 using UnityEngine;
 
 [ExecuteInEditMode]
+[SelectionBase]
 
-public class EditorSnap : MonoBehaviour
+public class CubeEditor : MonoBehaviour
 {
     [SerializeField] [Range(1f,20f)] float gridSize = 10f;
 
+    TextMesh textMesh;
 
- 
+    private void Start()
+    {
+        
+    }
+
     void Update ()
     {
         Vector3 snapPos;
         snapPos.x = Mathf.RoundToInt(transform.position.x /gridSize) * gridSize;
         snapPos.z = Mathf.RoundToInt(transform.position.z / gridSize) * gridSize;
+
+        
+        textMesh = GetComponentInChildren<TextMesh>();
+        textMesh.text = snapPos.x /gridSize + "," + snapPos.z/gridSize;
 
         transform.position = new Vector3(snapPos.x, 0f, snapPos.z);
 
